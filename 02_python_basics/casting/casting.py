3a38669a11c2dce0e975f29c8a6c80a80c3a1392
emmadidymus <emmanueldidymus@gmail.com>
2026-07-02 12:46:15 +0300

    Implicit and explict type conversions

---
+++ 02_python_basics/casting/casting.py
@@ -0,0 +1,32 @@
+a = int(1) # a will be 1
+print(a)
+
+b = int(2.8) # b will be 2
+print(b)
+
+c = int("3") # c will be 3
+print(c)
+
+
+
+x = float(1)
+print(x)
+
+y = float(2.8)
+print(y)
+
+z = float("3")
+print(z)
+
+w = float("4.2")
+print(w)
+
+
+d = str("s1")
+print(d)
+
+e = str(2)
+print(e)
+
+f = str(3.0)
+print(f)
