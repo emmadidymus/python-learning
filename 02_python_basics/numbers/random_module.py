4bf4b15fd416091dee74d3baba1826ad3156a450
emmadidymus <emmanueldidymus@gmail.com>
2026-07-02 12:30:17 +0300

    type conversions and the random module

---
+++ 02_python_basics/numbers/random_module.py
@@ -0,0 +1,8 @@
+"""
+Python does not have a random() function to make a random number,
+but it has a built-in random module that can be imported and used to make a random number.
+"""
+
+import random
+
+print(random.randrange(1, 1000))
---
+++ 02_python_basics/numbers/type_conversion.py
@@ -0,0 +1,20 @@
+#Type Conversion
+
+x = 5
+y = 3.14
+z = 5j
+
+#convert int to float
+a = float(x)
+print(a)
+print(type(a))
+
+#convert from float to integer
+b = int(y)
+print(b)
+print(type(b))
+
+#convert from int to complex
+c = complex(x)
+print(c)
+print(type(c))
