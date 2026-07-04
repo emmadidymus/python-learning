150cd5afda77065ff91964a5ebdbecb0bbf9429e
emmadidymus <emmanueldidymus@gmail.com>
2026-07-04 11:41:50 +0300

    copying python lists

---
+++ 03_collections/lists/copy_lists.py
@@ -0,0 +1,15 @@
+# using the copy() method
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+# using the list methode
+
+mylist = list(thislist)
+print(mylist)
+
+# using the slice operator
+
+mylist = thislist[:]
+print(mylist)
