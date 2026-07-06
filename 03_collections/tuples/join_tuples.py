db3ffe5cbf8265895e121eb931ae181ce0b9bec3
emmadidymus <emmanueldidymus@gmail.com>
2026-07-06 13:28:51 +0300

    joining python tuples

---
+++ 03_collections/tuples/join_tuples.py
@@ -0,0 +1,17 @@
+"""
+To join tuples into one tuple, You can use the + operator
+"""
+
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+
+"""
+You can use the * operator to duplicate the tuples content a given number of times
+"""
+tuple4 = (1, 2, 3)
+tuple4 *= 3
+print(tuple4)
