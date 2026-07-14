39af307fef3a7754c50df58f930a171f918274b5
emmadidymus <emmanueldidymus@gmail.com>
2026-07-14 13:10:26 +0300

    looping through python dictionaries

---
+++ 03_collections/dictionaries/looping_dictionaries.py
@@ -0,0 +1,24 @@
+y = {
+    "name":"John",
+    "age":28,
+    "married":True,
+    "children":True,
+    "divorced?":False,
+    "height":160,
+    "weight":80
+     }
+
+# Printing the keys
+
+for x in y.keys():
+    print(x)
+
+# Printing the values
+
+for x in y.values():
+    print(x)
+
+# Printing the items
+
+for x, z in y.items():
+    print(x,z)
