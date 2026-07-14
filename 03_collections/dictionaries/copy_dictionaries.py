b5ad83565a5e1c385efe14a85ada4698361e571f
emmadidymus <emmanueldidymus@gmail.com>
2026-07-14 13:14:46 +0300

    copying python dictionaries

---
+++ 03_collections/dictionaries/copy_dictionaries.py
@@ -0,0 +1,19 @@
+y = {
+    "name":"John",
+    "age":28,
+    "married":True,
+    "children":True,
+    "divorced?":False,
+    "height":160,
+    "weight":80
+     }
+
+#using the copy() method
+
+x = y.copy()
+print(x)
+
+#using the dict() method
+
+z = dict(y)
+print(z)
