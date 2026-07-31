988f54dc2b6a45d7867bf7889446b2f22181e5df
emmadidymus <emmanueldidymus@gmail.com>
2026-07-31 11:10:43 +0300

    python shorthand if

---
+++ 04_control_flow/if_else/shorthand_if.py
@@ -0,0 +1,28 @@
+# shorthand if
+a = 5
+b = 2
+if a > b: print("a is greater than b")
+
+#shorthand if.....else
+A = 5
+B = 2
+print("A") if A > B else print("B")
+
+#assigning a value with if...else
+c = 10
+d = 20
+
+bigger = c if c > d else d
+print(f"Bigger is {bigger}")
+
+#also
+x = 5
+y = 6
+max_value = x if x>y else y
+print(f"Max value is {max_value}")
+
+# multiple conditions on one line
+e = 330
+f = 330
+print ("e") if e > f else print("==") if e == f else print("f")
+
