166991b4c897c30b0b578549f41b6163a235d329
emmadidymus <emmanueldidymus@gmail.com>
2026-08-02 19:00:45 +0300

    python arguments

---
+++ 05_functions/basic_functions/arguments.py
@@ -0,0 +1,5 @@
+def family(fname, lname): #fname and lname are parameters
+    return fname + ' ' + lname
+
+print("Thobby", "Maguire") # "Thobby" and "Maguire" are arguments
+print("Harry", "Maguire")
