aec2d03a294473938249b1ee9bdab37ac8781606
emmadidymus <emmanueldidymus@gmail.com>
2026-08-02 19:47:52 +0300

    a dictionary as an argument in a python function

---
+++ 05_functions/basic_functions/a dictionary as an argument in a function.py
@@ -0,0 +1,9 @@
+def my_function(family):
+        print("Name:", family["name"])
+        print("Age:", family["age"])
+
+my_family ={
+    "name" : "Cherry",
+    "age" : 20
+}
+my_function(my_family)
