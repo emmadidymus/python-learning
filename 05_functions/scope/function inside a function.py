9252fe9dcc7c844d8099163d66ef7e8415efb2df
emmadidymus <emmanueldidymus@gmail.com>
2026-08-04 14:59:05 +0300

    accessing a local variable from a function inside a function

---
+++ 05_functions/scope/function inside a function.py
@@ -0,0 +1,8 @@
+def my_function():
+    x = 5
+    def myinnerfunction():
+          print(x)
+    myinnerfunction()
+my_function()
+
+# The local variable can be accessed from a function within a function
