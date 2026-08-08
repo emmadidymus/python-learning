5d28a3f4e97da5454ec1aa11d466f3749830a698
emmadidymus <emmanueldidymus@gmail.com>
2026-08-08 11:55:53 +0300

    the math module

---
+++ 06_modules/math/the math module.py
@@ -0,0 +1,19 @@
+import math
+
+"""
+The math.sqrt() method returns the square root of a number
+"""
+
+print(math.sqrt(81))
+
+"""
+The math.ceil() method rounds the number up to its nearest integer, 
+and math.floor() rounds the number down to its nearest integer and returns the results.
+"""
+
+print(math.ceil(1.4))
+print(math.floor(1.4))
+
+"""The math.pi constant returns the value of pi, 3.14....."""
+
+print(math.pi)
