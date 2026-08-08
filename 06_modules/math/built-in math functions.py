62105969ebd3bac4c3c5cbfcd5af9408a4216231
emmadidymus <emmanueldidymus@gmail.com>
2026-08-08 11:47:19 +0300

    built-in math functions

---
+++ 06_modules/math/built-in math functions.py
@@ -0,0 +1,19 @@
+"""
+The min() and max() functions can be used to find the lowest and highest values in an iterable.
+"""
+
+x = [34, 56, 23, 64, 63, 26]
+print(min(x))
+print(max(x))
+
+
+"""
+The abs() function returns the absolute (positive) number of a specified number
+"""
+
+print(abs(-4.56))
+
+"""
+pow(x,y) returns the value x to the power of y
+"""
+print(pow(2,3))
