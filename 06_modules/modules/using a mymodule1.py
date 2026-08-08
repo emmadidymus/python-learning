00f20199f30238c35a430cb75197aac6e789c7ae
emmadidymus <emmanueldidymus@gmail.com>
2026-08-08 10:33:23 +0300

    Renaming a module

--- 06_modules/modules/using a mymodule1.py
+++
@@ -1,4 +0,0 @@
-import mymodule1
-
-a = mymodule1.person1["name"]
-print(a)
---
+++ 06_modules/modules/using mymodule1.py
@@ -0,0 +1,10 @@
+import mymodule1
+
+a = mymodule1.person1["name"]
+print(a)
+
+# Renaming a module
+import mymodule1 as mx
+
+b = mymodule1.person1["Country"]
+print(b)
