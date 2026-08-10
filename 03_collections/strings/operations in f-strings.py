242ec994670769b9a6636ece6ed65b7ae40b08f1
emmadidymus <emmanueldidymus@gmail.com>
2026-08-10 12:18:29 +0300

    Operations in f-strings

---
+++ 03_collections/strings/operations in f-strings.py
@@ -0,0 +1,6 @@
+"""
+Adding taxes before displaying the price
+"""
+
+price = 59
+print(f"The price is {price*(price*0.18):.2f} dollars")
---
+++ 03_collections/strings/string formatting.py
@@ -0,0 +1,11 @@
+"""
+Placeholders and modifiers
+"""
+
+#Placeholder
+price = 59
+txt = f"The price is {price} dollars."
+print(txt)
+
+#Modifiers
+print(f"The price is {price:.2f} dollars!")
