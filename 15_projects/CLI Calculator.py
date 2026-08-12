567890ef97a3264465e5558015a3e4c5dc78393e
emmadidymus <emmanueldidymus@gmail.com>
2026-08-12 21:38:42 +0300

    adding the while loop to call the function again upon user request

--- 15_projects/CLI Calculator.py
+++ 15_projects/CLI Calculator.py
@@ -1,55 +1,64 @@
-print("=================")
-print("CLI CALCULATOR")
-print("=================")
-
-
-def result(x,y,z):
-    try:
-        if y == "+":
-            return x + z
-
-        elif y == "-":
-            return x - z
-
-        elif y == "*":
-            return x * z
-
-        elif y == "/":
-            return x / z
-
-        elif y == "%":
-            return x % z
-
-        elif y == "**":
-            return x ** z
-        return None
-    except ZeroDivisionError:
-        return "Error! Cannot divide by zero!"
-
-
-
 while True:
-    try:
-        x = float(input("Enter the first number: "))
+
+    print("=================")
+    print("CLI CALCULATOR")
+    print("=================")
+
+
+    def result(x, y, z):
+        try:
+            if y == "+":
+                return x + z
+
+            elif y == "-":
+                return x - z
+
+            elif y == "*":
+                return x * z
+
+            elif y == "/":
+                return x / z
+
+            elif y == "%":
+                return x % z
+
+            elif y == "**":
+                return x ** z
+            return None
+        except ZeroDivisionError:
+            return "Error! Cannot divide by zero!"
+
+
+    while True:
+        try:
+            x = float(input("Enter the first number: "))
+            break
+        except ValueError:
+            print("Please enter a valid number!")
+
+    while True:
+        try:
+            z = float(input("Enter the second number: "))
+            break
+        except ValueError:
+            print("Please enter a valid number!")
+
+    while True:
+        y = input("Enter Operator(+, -, *, /, %, **): ")
+
+        if y in ["+", "-", "*", "/", "%", "**"]:
+            break
+
+        print("Please enter a valid operator!")
+
+    print("Result: ", result(x, y, z))
+
+    repeat = input("Calculate again? (Yes/No): ")
+
+    if repeat.lower() == "yes":
+        continue
+    else:
         break
-    except ValueError:
-        print("Please enter a valid number!")
-
-while True:
-    try:
-        z = float(input("Enter the second number: "))
-        break
-    except ValueError:
-        print("Please enter a valid number!")
 
 
-while True:
-    y = input("Enter Operator(+, -, *, /, %, **): ")
-
-    if y in ["+", "-", "*", "/", "%", "**"]:
-        break
-
-    print("Please enter a valid operator!")
-
-print("Result: ", result(x,y,z))
 
