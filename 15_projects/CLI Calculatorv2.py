885cd7f70d7c8b31f8462c38407d2bdca2755611
emmadidymus <emmanueldidymus@gmail.com>
2026-08-13 15:20:57 +0300

    CLI calculator: placing the function call outside of the while loop

--- 15_projects/CLI Calculatorv2.py
+++ 15_projects/CLI Calculatorv2.py
@@ -1,11 +1,30 @@
 import math
 
+
+def results(x, y, z):
+    try:
+        if y == '+':
+            return x + z
+        elif y == '-':
+            return x - z
+        elif y == '*':
+            return x * z
+        elif y == '/':
+            return x / z
+        elif y == '%':
+            return x % z
+        elif y == '**':
+            return x ** z
+    except ZeroDivisionError:
+        return "Error! Cannot divide by zero!"
+
 while True:
     print("=================")
     print("CLI CALCULATOR")
     print("=================")
 
     while True:
+
         y = input("Enter Operator(+, -, *, /, %, **,sqrt): ")
 
         if y in ["+", "-", "*", "/", "%", "**"]:
@@ -24,51 +43,33 @@ while True:
                     print("Please enter a valid number!")
 
 
-            def results(x, y, z):
-                try:
-                    if y == '+':
-                        return x + z
-                    elif y == '-':
-                        return x - z
-                    elif y == '*':
-                        return x * z
-                    elif y == '/':
-                        return x / z
-                    elif y == '%':
-                        return x % z
-                    elif y == '**':
-                        return x ** z
-                except ZeroDivisionError:
-                    return "Error! Cannot divide by zero!"
-
-
             print("Result:", results(x, y, z))
+            break
 
 
         elif y in ["sqrt"]:
             while True:
                 try:
                     a = float(input("Enter a number: "))
+                    print("The square root is:", math.sqrt(a))
                     break
                 except ValueError:
-                    print("Please enter a valid number!")
+                    print("Please enter a nonnegative number!")
 
 
-            def sqrt(a):
-                return math.sqrt(a)
-
-
-            print("The square root is:", sqrt(a))
         else:
             print("Please enter a valid operator!")
 
-        break
+    repeat = input("Calculate again? (Yes/No): ")
 
-    repeat = input("Calculate again?(Yes/No): ")
-
-    if repeat.lower() == "yes":
+    if repeat.lower() == 'yes':
         continue
     else:
         break
+
+
+
+
+
 
 
